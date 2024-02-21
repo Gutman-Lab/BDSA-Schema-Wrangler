501a40d5e6db0a8d3b1c5bdbaa9d4bc618c04c48
JC Vizcarra <jvizcar@emory.edu>
2024-02-21 18:34:04 +0000

    script to generate sample Emory metadata csv

---
+++ scripts/emory-csv-file.py
@@ -0,0 +1,46 @@
+"""Create a metadata CSV file for a sample of Emory's ADRC DSA collection."""
+
+from girder_client import GirderClient
+from pathlib import Path
+import pandas as pd
+
+
+def main():
+    # Load environment variables from .env
+    gc = GirderClient(apiUrl="https://megabrain.neurology.emory.edu/api/v1")
+    print('Authenticate girder client for "https://megabrain.neurology.emory.edu"\n')
+    gc.authenticate(interactive=True)
+
+    # We sample the year 2015 - list all these items.
+    metadata = []
+
+    for item in gc.get(
+        "resource/641ba814867536bb7a225533/items?type=collection&limit=0"
+    ):
+        # Grab the associated metadata for each item.
+        np_schema = item.get("meta", {}).get("npSchema", {})
+
+        metadata.append(
+            [
+                item["name"],
+                np_schema.get("caseID", ""),
+                np_schema.get("stainID", ""),
+                np_schema.get("regionName", ""),
+                np_schema.get("blockID", ""),
+            ]
+        )
+
+    # Format as dataframe and save to CSV.
+    df = pd.DataFrame(
+        metadata, columns=["fileName", "caseID", "stainID", "regionName", "blockID"]
+    )
+    df = df.sort_values(by="fileName")
+
+    Path("metadata").mkdir(exist_ok=True)
+    df.to_csv("metadata/emory-sample.csv", index=False)
+
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
